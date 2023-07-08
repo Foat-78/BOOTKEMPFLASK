08ab76c5d8a41dba8cdebf4da0c649ba2e443609
Foat Khisamov <foat.com>
2023-07-08 19:18:52 +1000

    Знакомство с Flask

--- main.py
+++ main.py
@@ -8,9 +8,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'Hello, Foat'
+def main():
+    return "<h1>Hello, Foat</h1><br><a href='/index'>перейти на 2-ую страницу</a>"
 
+# @app.route('/index')
+# def index():
+#     return "It's my first project"
+
+@app.route('/index/<x>/<y>')
+def index(x, y):
+    return f'Результат: {int(x) + int(y)}' 
 
 if __name__=='__main__':
     app.run()
+
+    
